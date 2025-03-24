88ea3859be00738e721759f09af2be81f0dc9c61
Furkan Akarçeşme <furkanakarcesme@gmail.com>
2025-03-25 01:13:50 +0300

    Service implementation for two entities

--- Repositories/EFCore/AvailabilityRepository.cs
+++ Repositories/EFCore/AvailabilityRepository.cs
@@ -13,23 +13,7 @@ public class AvailabilityRepository : IAvailabilityRepository
     {
         _context = context;
     }
-
-    /*
-    public async Task<List<Availability>> GetAvailabilitiesWithLazyCreation(int doctorId, DateTime start, DateTime end)
-    {
-        var existing = await _context.Availabilities
-            .Where(a => a.DoctorId == doctorId && a.AvailableDate >= start && a.AvailableDate <= end)
-            .ToListAsync();
-
-        if (existing.Any())
-            return existing;
-
-        var newSlots = SlotGenerator.GenerateSlotsForDoctor(doctorId, start, end); // static helper
-        await _context.Availabilities.AddRangeAsync(newSlots);
-        await _context.SaveChangesAsync();
-        return newSlots;
-    }
-    */
+    
 
     public async Task<List<Availability>> GetAvailabilitiesWithLazyCreation(int doctorId, DateTime start, DateTime end)
     {
@@ -61,33 +45,8 @@ public class AvailabilityRepository : IAvailabilityRepository
     }
     
     
-    
-    /*
     public async Task<Availability?> GetAvailabilityByDoctorAndTime(int doctorId, DateTime date, TimeSpan startTime)
     {
-        /*return await _context.Availabilities
-            .FirstOrDefaultAsync(a => a.DoctorId == doctorId 
-                                      && a.AvailableDate == date 
-                                      && a.StartTime == startTime);
-    
-    
-        return await _context.Availabilities
-            .FirstOrDefaultAsync(a => 
-                a.DoctorId == doctorId &&
-                a.AvailableDate.Date == date.Date && // Sadece gün kısmını kontrol et
-                a.StartTime == startTime && // JSON stringini TimeSpan'e çevir
-                a.IsBooked == false);
-    }
-    */
-    
-    public async Task<Availability?> GetAvailabilityByDoctorAndTime(int doctorId, DateTime date, TimeSpan startTime)
-    {
-        // Sadece gün bazında karşılaştır: AvailableDate.Date == date.Date
-        // Saat – dakika bazında karşılaştır: 
-        //   a.StartTime.Hours == startTime.Hours 
-        //   a.StartTime.Minutes == startTime.Minutes
-        // Bu şekilde milisaniye, format vb. farkları bertaraf ediyoruz.
-
         return await _context.Availabilities
             .FirstOrDefaultAsync(a => 
                 a.DoctorId == doctorId &&
--- Services/Contracts/IAvailabilityService.cs
+++ Services/Contracts/IAvailabilityService.cs
@@ -1,6 +1,9 @@
+using Entities.DataTransferObjects;
+
 namespace Services.Contracts;
 
 public interface IAvailabilityService
 {
-    
+    Task<List<AvailabilityDto>> GetAvailableSlots(int doctorId, DateTime startDate, DateTime endDate);
+
 }
